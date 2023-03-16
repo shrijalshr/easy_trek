39942bb4e2ef922f1346521e26f250072f52258f
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-17 00:20:16 +0545

    added img path

--- lib/core/constants/assets_path.dart
+++ lib/core/constants/assets_path.dart
@@ -2,4 +2,9 @@ class AssetPaths {
   AssetPaths._();
   static const String _iconPath = "assets/icons";
   static const String _imagePath = "assets/images";
+
+  static const String logo = "$_imagePath/logo.png";
+  static const String mountain1 = "$_imagePath/mountain1.png";
+  static const String mountain2 = "$_imagePath/mountain2.png";
+  static const String mountain4 = "$_imagePath/mountain4.png";
 }
