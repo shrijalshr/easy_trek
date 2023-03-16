94a491313873a22129637af92e040de5e6f8ba96
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-16 22:24:08 +0545

    created routes and routes generator

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,14 +1,43 @@
+import 'package:easy_trek/module/auth/login/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
-class Routes{
-
+class Routes {
+  Routes._();
+  static const String onBoarding = "/onBoarding";
+  static const String login = "/login";
+  static const String signup = "/signup";
+  static const String homescreen = "/homescreen";
 }
 
+class RouteGenerator {
+  static Route<dynamic> onRouteGenerate(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return getPage(const LoginScreen());
 
-class RouteGenerator{
-  static onRouteGenerate(RouteSettings settings)
-{
+      case Routes.login:
+        return getPage(const LoginScreen());
+      default:
+        return getPage(const LoginScreen());
+    }
+  }
 
+  static getPage(page) {
+    return PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => page,
+        transitionDuration: const Duration(seconds: 1),
+        transitionsBuilder: (_, a, __, c) {
+          const begin = Offset(-1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.ease;
 
-  
-}}
+          final tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+          return SlideTransition(
+            position: a.drive(tween),
+            child: c,
+          );
+        });
+  }
+}
