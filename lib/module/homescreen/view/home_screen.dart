10da87e652a5166eb18e29aded4fe090dedf5d2c
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-20 19:40:03 +0545

    added cards and tabs to homescreen

--- lib/module/homescreen/view/home_screen.dart
+++ lib/module/homescreen/view/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:easy_trek/core/export.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../widget/trek_card.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
@@ -37,6 +40,7 @@ class Homescreen extends StatelessWidget {
             Expanded(
                 child: TabBarView(children: [
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(
                     height: 40.fh,
@@ -45,16 +49,56 @@ class Homescreen extends StatelessWidget {
                       scrollDirection: Axis.horizontal,
                       child: Row(
                         children: [
-                          const TrekCard().pl(16),
-                          const TrekCard().pl(16),
-                          const TrekCard().pl(16),
-                          const TrekCard().pl(16),
+                          const TrekCard().pr(16),
+                          const TrekCard().pr(16),
+                          const TrekCard().pr(16),
+                          const TrekCard().pr(16),
+                        ],
+                      ),
+                    ),
+                  ).pb(28),
+                  Text(
+                    "Top Destination",
+                    style: context.textStyles.titleLarge,
+                  ).pb(16),
+                  GestureDetector(
+                    onTap: () {
+                      Get.toNamed(Routes.placeDetailScreen);
+                    },
+                    child: Container(
+                      height: 10.fh,
+                      width: 60.fw,
+                      decoration:
+                          BoxDecoration(color: context.color.warningColor),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Image.asset(
+                              AssetPaths.mountain4,
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Mt. Everest",
+                                  style: context.textStyles.headlineSmall,
+                                ),
+                                Text(
+                                  "Sagarmatha",
+                                  style: context.textStyles.displayMedium,
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
                     ),
                   )
                 ],
-              ),
+              ).ph(16),
               Column(
                 children: const [Text("Jungle")],
               ),
@@ -65,98 +109,6 @@ class Homescreen extends StatelessWidget {
                 children: const [Text("Beach")],
               ),
             ]))
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class TrekCard extends StatelessWidget {
-  const TrekCard({
-    super.key,
-  });
-
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(8),
-      child: Container(
-        child: Stack(
-          children: [
-            SizedBox(
-              height: 40.fh,
-              width: 52.fw,
-              child: Image.asset(
-                AssetPaths.mountain1,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Positioned(
-              bottom: 20,
-              child: SizedBox(
-                width: 52.fw,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(
-                        right: 32,
-                      ),
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      decoration: BoxDecoration(
-                        color: context.color.white,
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(8),
-                          topRight: Radius.circular(8),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Icon(
-                            Icons.star_border_outlined,
-                            size: 12,
-                          ),
-                          Text(
-                            "4.8",
-                            style: context.textStyles.titleSmall
-                                ?.copyWith(fontSize: 8),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      decoration: BoxDecoration(
-                        color: context.color.white,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Machhapuchhare",
-                            style: context.textStyles.displaySmall!
-                                .copyWith(fontWeight: FontWeight.w900),
-                          ),
-                          Text("Annapurna massif of Gandaki Province",
-                              style: context.textStyles.bodySmall!.copyWith(
-                                fontSize: 8,
-                              ))
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
           ],
         ),
       ),
