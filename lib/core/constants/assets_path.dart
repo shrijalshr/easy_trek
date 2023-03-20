9bb3720863a6fd253dc8575e9bba334794290b74
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-20 18:48:51 +0545

    added ghandruk photo

--- lib/core/constants/assets_path.dart
+++ lib/core/constants/assets_path.dart
@@ -4,6 +4,7 @@ class AssetPaths {
   static const String _imagePath = "assets/images";
 
   static const String logo = "$_imagePath/logo.png";
+  static const String ghandruk = "$_imagePath/ghandruk.png";
   static const String mountain1 = "$_imagePath/mountain1.png";
   static const String mountain2 = "$_imagePath/mountain2.png";
   static const String mountain4 = "$_imagePath/mountain4.png";
