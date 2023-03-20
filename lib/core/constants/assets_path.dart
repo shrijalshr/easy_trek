f9a91c0f3ec1d8b2aed03f2f094ee8872f414102
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-20 19:38:57 +0545

    new images

--- lib/core/constants/assets_path.dart
+++ lib/core/constants/assets_path.dart
@@ -5,6 +5,10 @@ class AssetPaths {
 
   static const String logo = "$_imagePath/logo.png";
   static const String ghandruk = "$_imagePath/ghandruk.png";
+  static const String ghandruk1 = "$_imagePath/ghandruk1.jpeg";
+  static const String ghandruk2 = "$_imagePath/ghandruk2.jpg";
+  static const String ghandruk3 = "$_imagePath/ghandruk3.jpg";
+  static const String ghandruk4 = "$_imagePath/ghandruk4.jpg";
   static const String mountain1 = "$_imagePath/mountain1.png";
   static const String mountain2 = "$_imagePath/mountain2.png";
   static const String mountain4 = "$_imagePath/mountain4.png";
